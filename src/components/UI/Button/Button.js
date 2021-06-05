9d11ad9d5794ad16c18a2385522da4df32bbf9db
Pedro Batista <pedromachakio@hotmail.com>
2021-06-05 22:21:27 +0000

    changed the stylign from styled components lib to css modules which I find better to keep components lean and css on a different file

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,6 +1,9 @@
-import styled from "styled-components";
+//import styled from "styled-components";
+import styles from "./Button.module.css"; // para trabalhar com css modules, em vez de importar o ficheiro de css
+// "normalmente" importa-se assim (o styles pode ser whatever). Para além disso é preciso rename o ficheiro de css para incluir module entre o nome e a
+// extensão
 
-const Button = styled.button`
+/* const Button = styled.button`
   width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
@@ -24,14 +27,14 @@ const Button = styled.button`
     border-color: #ac0e77;
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
   }
-`;
+`; */
 
-/* const Button = props => {
+const Button = (props) => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
-}; */
+};
 
 export default Button;
